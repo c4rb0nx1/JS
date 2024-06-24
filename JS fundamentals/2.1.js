fedb07476e031b64af634a5095831dcb5c680bfc
Niranchan <90444898+c4rb0nx1@users.noreply.github.com>
2024-06-24 18:02:54 +0530

    2.1

---
+++ JS fundamentals/2.1.js
@@ -0,0 +1,4 @@
+//alert('hello'); <--- only applicable on browsers
+"use strict"; // used to enfore use of modern way js | always must br on top ...otherwise ignored 
+console.log('hello world');
+https://github.com/alex/what-happens-when?tab=readme-ov-file#http-protocol
