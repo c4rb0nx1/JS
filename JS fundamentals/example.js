2c859a0b4e061be0ae10df3668c65391e1668ef0
Niranchan <90444898+c4rb0nx1@users.noreply.github.com>
2024-06-27 14:10:23 +0530

    completed - JS example 1, objects

--- JS fundamentals/example.js
+++ JS fundamentals/example.js
@@ -1,47 +1,89 @@
 // IT support CLI
 const prompt = require('prompt-sync')();
 
+
+
+console.log("!-------| Asset Checker |\n");
 let name = prompt("Enter your name: ");
 let empID = prompt("Your Employee ID here: ");
 let empCat = empID.slice(0,3)
 let empDep = empID.slice(3,);
 
-let internArr = ['000','112','123'];
-let empArr = ['000','321']
+// object creation - start
+let details;
+let niranObj = {
+    name : "Niranchan D",
+    age : 21,
+    id : "INT112",
+    startDate : "18.06.2024",
+    endDate : "30.04.2024",
+    role : "intern - Devops engineer",
+    assets : ["MacBook Pro M1 (SSLAP112)","Monitor - benq (SS207)", "Mouse - Logitech (SS208)"],
+};
 
-// case: to check if the emp id exists on a DB and return when found.
+let abhiObj = {
+    name : "Abhinandan G",
+    age : 20,
+    id : "INT137",
+    startDate : "18.06.2024",
+    endDate : "30.04.2024",
+    role : "intern - Product Developer",
+    assets : ["MacBook Pro M1 (SSLAP137)","Monitor - benq (SS201)"],
+};
+
+let aswinObj = {
+    name : "aswin kumar",
+    age : 26,
+    id : "EMP007",
+    startDate : null,
+    endDate : null,
+    role : "senior Devops Engineer",
+    assets : ["MacBook ultra pro max ultra (SSLAP007)","Monitor - benq (SS207)", "Mouse - Logitech (SS208)"],
+};
+// object creation - end
+
+//obj array to maintain all the employee details.
+let empDetObjArr = [niranObj, abhiObj,aswinObj];
+
+// func dec - start. 
+let defaultError = () => {
+    console.log("user",name,"has no matching record with",empID);
+}
+// func to check wether the INT id is ther r not.
+let check = () => { 
+    for(let k in empDetObjArr){
+        details = empDetObjArr.find(fun => fun.id == empID);
+    }
+    return !!details; // something newwww
+}
+
+
+
+// -----function dec end .
 let empType = async () => {
     setTimeout(() => {
         switch(empCat){ // using set timeout to simulate a fetching delay.
         case 'INT':
-            if (internArr.includes(empDep)) {
-                if(empDep > 100 && empDep <= 200){
-                    console.log("Status: DevOps Intern");
-                }else if(empDep > 200 && empDep <=300){
-                    console.log("Status: Product development");
-                }
+            if(check()){ // check returns tru or fls after checking
+                console.log("success", details.assets);
             }
             break;
         case 'EMP':
-            if(empArr.includes(empDep)){
-                if(empDep > 100 && empDep <= 200){
-                    console.log("Status: DevOps Engineer");
-                }else if(empDep > 200 && empDep <=300){
-                    console.log("Status: Product Dev");
-                }
+            if(check()){
+                console.log("Success", details.assets);
             }
             break;   
         default:
             console.log("Not registred");
 
         }
-    },5000);
+    },1000);
+    let office = prompt("Your office locaiton: ");
 }
 
 
 if(empID.length == 6){
     empType();
-    let office = prompt("Your office locaiton: ");
 }else{
     console.log("Invalid Employee ID");
     empID = prompt("Your Employee ID here: ");
---
+++ JS fundamentals/obects.js
@@ -0,0 +1,50 @@
+// let sampleObject = {
+//     naaam : "niranchan",
+//     age : 21,
+// };
+
+// let freeVar = "fr";
+
+// let computerObj = {
+//     [freeVar]: "free",
+// }
+// console.log(computerObj.fr.slice(0,3));
+
+// freeVar = "notfr";
+
+// console.log(computerObj["notfr"]); // output's undefined because " computed props assign keys from the variable at the time of creation of objects"
+
+
+// console.log(sampleObject.naaam);
+
+
+const niranObj = {
+    name : "Niranchan D",
+    age : 21,
+    id : "INT112",
+    startDate : "18.06.2024",
+    endDate : "30.04.2024",
+};
+
+let chanObj = {
+    name : "Niranchan D",
+    age : 21,
+    id : "INT112",
+    startDate : "18.06.2024",
+    endDate : "30.04.2024",
+};
+
+let arrObj = [niranObj, chanObj];
+
+console.log(arrObj[niranObj.id]);
+
+// object references:
+let newOBj  = niranObj;
+
+console.log(newOBj, niranObj);
+
+//after change to new obj
+
+newOBj.id = "INT000";
+
+console.log(newOBj, niranObj);
