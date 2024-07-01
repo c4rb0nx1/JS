b61730f8f4dcfc4f1ba0eb00d9cfd07ac6fb117f
Niranchan <90444898+c4rb0nx1@users.noreply.github.com>
2024-07-01 17:21:57 +0530

    started node JS

---
+++ JS fundamentals/node js/sample.js
@@ -0,0 +1,11 @@
+// console.log("hw");
+let http = require("http")
+
+http.createServer((req,res)=>{
+    console.log("server running on localhost:8080")
+    res.writeHead(200,{
+        'content-type':'text/html'
+    })
+    res.write("hello form node") // use when multiple needs to be sent and 
+    res.end() // use directly end like res.end("hello from node") for a single msg
+}).listen(8080)
