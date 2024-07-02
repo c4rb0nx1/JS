fc4c0d851fee6dd36beb3d89a2bdebca4a89540b
Niranchan <90444898+c4rb0nx1@users.noreply.github.com>
2024-07-02 13:11:08 +0530

    express js -> view engine ejs, router

---
+++ JS fundamentals/experss js/routes/users.js
@@ -0,0 +1,38 @@
+const express = require('express')
+
+const router = express.Router()
+
+
+router.route("/:id")
+    .get((req,res)=>{
+    console.log("get trig")
+    res.send("recieved a get req for id"+req.params.id)
+})
+    .post((req,res)=>{
+    console.log('post trig' + req.params.id)
+    res.send("Recieved a post req for "+req.params.id)
+})
+    .delete((req,res)=>{
+    console.log("delete trig")
+    res.send(`User ${req.params.id} is deleted`)
+})
+
+//the above is the below equivalent code :)
+
+
+// router.get("/",(req,res)=>{
+//     console.log("test hit")
+//     res.send("test endpoint")
+// })
+
+
+// router.get("/:id",(req,res)=>{
+//     console.log("test hit at "+ req.params.id)
+//     res.send("test endpoint "+ req.params.id)
+// })
+
+// router.get("/params",(res,req)=>{
+//     console.log("params test endpoint")
+// })
+
+module.exports = router
